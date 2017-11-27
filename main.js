ac1a26f6cc0fdaa455d922b6bd3cd884f0e61e81
florent <florent@recisio.com>
2017-11-28 00:07:50 +0100

    use Station and StationManager

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+class Station {
+    constructor(){
+        this.id;
+        this.name;
+        this.state;
+        this.adress;
+        this.nbBikes;
+        this.nbSlots;
+        this.acceptCb;
+        this.gps;
+    }
+}
+
+class StationManager {
+    static loadFromVlilleApi(json) {
+        const station = new Station();
+        station.id = json.fields.libelle;
+        station.name = json.fields.nom.replace(json.fields.libelle,'').replace('(CB)','').trim();
+        station.state = json.fields.etat + '/' + json.fields.etatConnexion;
+        station.adress = json.fields.adresse + ' ' + json.fields.commune;
+        station.nbBikes = json.fields.nbVelosDispo;
+        station.nbSlots = json.fields.nbPlacesDispo;
+        station.acceptCb = json.fields.type === "AVEC TPE";
+        station.gps = json.fields.geo;
+        return station;
+    }
+}
+
 let vlille = {
     nbStations: 0,
     stations: {}
@@ -8,8 +36,8 @@ function updateVlilleStations() {
         .then(response => response.json())
         .then((json) => {
             vlille.nbStations = json.nhits;
-            vlille.stations = json.records;
-            console.log(vlille);
+            vlille.stations = json.records.map(record => StationManager.loadFromVlilleApi(record));
+            console.log(vlille.stations);
         });
 }
 
@@ -22,4 +50,3 @@ const app = new Vue({
         vlille
     },
 });
-console.log(app);
